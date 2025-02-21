402bda1aff02f8c6d397b57fcecfb395360e0b26
mohamedhw <mohamedhw2000@gmail.com>
2025-02-21 14:30:03 +0200

    feat: added the CRUD for google drive

---
+++ utils/drive/pix_drive.js
@@ -0,0 +1,90 @@
+const { google } = require('googleapis');
+const fs = require('node:fs');
+const path = require('path');
+const dotenv = require("dotenv");
+
+const filePath = path.join(__dirname, '../../.env');
+const envParams = dotenv.config({ path: filePath }).parsed;
+
+const CLIENT_ID = envParams.CLIENT_ID;
+const CLIENT_SECRET = envParams.CLIENT_SECRET;
+const REDIRECT_URL = envParams.REDIRECT_URL;
+
+const REFRESH_TOKEN = envParams.REFRESH_TOKEN;
+
+const oauth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URL
+);
+
+oauth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+
+const drive = google.drive({
+  version: 'v3',
+  auth: oauth2Client
+});
+
+// const filePath = path.join(__dirname, 'test.jpg');
+
+async function uploadFile(image) {
+  const filePath = path.join('../../', image);
+  try {
+    const response = await drive.files.create({
+      requestBody: {
+        name: image,
+        mimeType: 'image/jpg',
+        parents: ['17F3zxq0a8XL4WgJFk_P1muQn_ql54l4P'],
+      },
+      media: {
+        mimeType: 'image/jpg',
+        body: fs.createReadStream(filePath)
+      }
+    })
+    return response.data.id;
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+
+// uploadFile();
+
+
+async function deleteFile() {
+  try {
+    const response = await drive.files.delete({
+      fileId: '1-EGhfjtwLORxdopMHl6Bm7uiY5e0_rcd',
+    })
+    console.log(response.data, response.status)
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+
+// deleteFile();
+
+
+async function generatePublicUrl(image) {
+  try {
+    const fileId = await uploadFile(image);
+    await drive.permissions.create({
+      fileId: fileId,
+      requestBody: { role: 'reader', type: 'anyone' }
+    });
+
+    const result = await drive.files.get({
+      fileId: fileId,
+      fields: 'webViewLink, webContentLink'
+    });
+
+    let src = `https://lh3.googleusercontent.com/d/${result.data.webViewLink.split('/')[5]}=w1000?authuser=1/view`;
+    return src;
+  } catch (err) { console.log(err.message) }
+}
+// we need to make the url in this format 
+// src="https://lh3.googleusercontent.com/d/'ID'=w1000?authuser=1/view"
+generatePublicUrl('test.jpg').then((url) => console.log(url));
+
+
