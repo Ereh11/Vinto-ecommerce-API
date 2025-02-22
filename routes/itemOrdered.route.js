f5a2f262372eb898fde008d5300c55ef22452546
‪Hany Abdou Saad‬‏ <Hanysaadstd@gmail.com>
2025-02-22 21:38:51 +0200

    feat: update itemOrdered validation and refactor database connection

--- routes/itemOrdered.route.js
+++ routes/itemOrdered.route.js
@@ -2,15 +2,15 @@ const express = require("express");
 const router = express.Router();
 const itemOrderedController = require("../controllers/itemOrdered.controllers");
 const validateItemOrderedSchema = require("../middlewares/validateItemOrdered");
-const validateItemOrderedID = require("../middlewares/validateItemOrderedID");
+const validateItemID = require("../middlewares/validateItemID");
 
 router.route("/")
     .post(validateItemOrderedSchema, itemOrderedController.createItemOrdered)
     .get(itemOrderedController.getAllItemOrdered)
     .delete(itemOrderedController.deleteAllItemOrdered);
 router.route("/:id")
-    .get(validateItemOrderedID, itemOrderedController.getItemOrderedById)
-    .delete(validateItemOrderedID, itemOrderedController.deleteItemOrderedById)
-    .put(validateItemOrderedID, validateItemOrderedSchema, itemOrderedController.updateItemOrderedById)
-    .patch(validateItemOrderedID, validateItemOrderedSchema, itemOrderedController.updatePartialyItemOrderedById);
+    .get(validateItemID, itemOrderedController.getItemOrderedById)
+    .delete(validateItemID, itemOrderedController.deleteItemOrderedById)
+    .put(validateItemID, validateItemOrderedSchema, itemOrderedController.updateItemOrderedById)
+    .patch(validateItemID, validateItemOrderedSchema, itemOrderedController.updatePartialyItemOrderedById);
 module.exports = router;
