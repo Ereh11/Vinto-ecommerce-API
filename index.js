78f7e12b8da866b270d053d03233d93383b0387c
KholoudELkasaby <lilelksaby@gmail.com>
2025-02-22 22:01:09 +0200

    fix: update MongoDB connection string to use environment variables

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
-// Main file to start our API
-
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const categoryRoutes = require("./routes/category.route.js");
 const errorHandler = require("./middlewares/errorHandler.js");
 
-const URLDB =
-  "mongodb+srv://Vintodevs:amj76CzcY4Ymqeqc@vintocluster.frlbn.mongodb.net/?retryWrites=true&w=majority&appName=VintoCluster";
-
 const app = express();
 
+const MONGODB_URI = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_NAME}:27017,${process.env.CLUSTER_NAME}:27017,${process.env.CLUSTER_NAME}:27017/${process.env.DB_NAME}?ssl=true&replicaSet=atlas-7o5bfh-shard-0&authSource=admin&retryWrites=true&w=majority&appName=${process.env.APP_NAME};`;
+
 mongoose
-  .connect(URLDB)
-  .then(() => {
-    console.log("Mongoose Connect Successfully");
-  })
-  .catch((err) => {
-    console.log("ERORRRRRRRRR");
-  });
+  .connect(MONGODB_URI)
+  .then(() => console.log(" Mongoose Connected Successfully"))
+  .catch((err) => console.error("ERORR"));
 
 app.use(cors());
 app.use(express.json());
 
-// Here put your routes
-// app.use("", );
 app.use("/api/categories", categoryRoutes);
 app.use(errorHandler);
 
 app.listen(4000, () => {
-  console.log("Server is listining on port 4000");
+  console.log("Server is listening on port 4000");
 });
